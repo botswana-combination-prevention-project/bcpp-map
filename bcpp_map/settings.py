8d658c97f89a9a8b9843404e9243f05d8111af74
erikvw <ew2789@gmail.com>
2016-12-22 00:00:24 +0200

    update migrations

--- bcpp_map/settings.py
+++ bcpp_map/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bcpp_map.apps.BcppMapAppConfig',
+    'edc_device.apps.AppConfig',
+    'example_survey.apps.EdcMapAppConfig',
+    'bcpp_map.apps.AppConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-CURRENT_SURVEY = 'bcpp-year-1'
-CURRENT_COMMUNITY = None
-GIT_DIR = BASE_DIR
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+KEY_PATH = os.path.join(BASE_DIR, 'crypto_fields')
+
+CURRENT_MAP_AREA = None
+
+if 'test' in sys.argv:
+    DEVICE_ID = '99'
+    CURRENT_MAP_AREA = 'test_community'
